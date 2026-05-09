d28ef699224451a092ee6ffeb74db7ec6d312228
Katia <karnayh1111111@gmail.com>
2026-05-09 17:22:28 +0300

    port-2

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
 
 // ─── Start Server ─────────────────────────────────────────────────────────────
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
